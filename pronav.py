84cddc94e0b7f10127b65c60b69b16ade901782e
ngojaden0 <ngojaden0@gmail.com>
2023-10-05 22:29:47 -0500

    simple matplotlib program

--- pronav.py
+++ pronav.py
@@ -1,5 +1,19 @@
-print("hello")
-a = 5
-b = 7
-c = a+b
-print(c)
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Create a figure and axis
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, marker='o', linestyle='-')
+
+# Add labels and a title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Line Plot')
+
+# Display the plot
+plt.grid(True)
+plt.show()
+
